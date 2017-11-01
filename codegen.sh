49c225b32102926c86ecfaa20460bf38b80af8a1
Matthew Longley <randomticktock@gmail.com>
2017-11-01 14:03:40 -0500

    Finished adding lines in codegen script

--- codegen.sh
+++ codegen.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-java -jar "$SWAGGER_CODEGEN_PATH" {$@}
+java -jar "$SWAGGER_CODEGEN_PATH" generate -i schemas/swagger.yaml -l javascript clients/binance-javascript
+java -jar "$SWAGGER_CODEGEN_PATH" generate -i schemas/swagger.yaml -l python clients/binance-python
